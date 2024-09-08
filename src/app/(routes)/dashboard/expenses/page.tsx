062836ac70179c00e287b9c2d763da830b71b7ea
abubakarnangrii <abubakarnangri@gmail.com>
2024-09-08 10:46:41 +0500

    almost frontend ready

--- src/app/(routes)/dashboard/expenses/page.tsx
+++ src/app/(routes)/dashboard/expenses/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import Breadcrumb from '@/components/Breadcrumb';
-import React from 'react'
+import Breadcrumb from "@/components/Breadcrumb";
+import ExpensesTable from "@/components/ExpensesTable";
+import React from "react";
 
 const Expenses = () => {
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <div className="flex flex-col justify-center items-center py-4 gap-2">
-      <h1 className="text-3xl font-semibold">My Expenses</h1>
-      <Breadcrumb name={"Expenses"} link={'expenses'}/>
+        <h1 className="text-3xl font-semibold">My Expenses</h1>
+        <Breadcrumb name={"Expenses"} link={"expenses"} />
+      </div>
+      <div>
+        <h1 className="text-xl font-semibold pb-3">Latest Expenses</h1>
+        <ExpensesTable />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Expenses
+export default Expenses;
